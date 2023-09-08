1c3d6b0ea959477a48531f56945375b822a0d391
TomasdeA <tdeaguirre@fi.uba.ar>
2023-09-08 00:35:23 +0000

    comments, gpio

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
+/**
+ * @brief Al ejemplo de ARM https://github.com/armBookCodeExamples/example_1-3
+ *  
+ * @arg none
+ * @param gasDetector El pin al que se conecta el sensor de gas (ahora un switch a 3v3)
+ * @param overTempDetector El pin al que se conecta el sensor de temperatura (ahora un switch a 3v3)
+ * @param alarmLed El LED el cual se usa como alarma.
+ * @param alarmOffButton El pin al que se conecta el botón para apagar la alarma (ahora un switch a 3v3)
+ * 
+ * @note Asegúrese de que esté el button/switch conectado a 3v3 y a los pines D2 y D3.
+ * @note Se utiliza un pull down por ende se debe conectar a 3v3. 
+ * Si se pone Pull up, conectar a GND
+ * 
+ * @retval none.
+ */
+//%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 #include "mbed.h"
 #include "arm_book_lib.h"
 
@@ -11,7 +27,6 @@ int main()
     
     gpio_init_out(&alarmLED,LED1);
     DigitalOut alarmLed(LED1);
-
 
     alarmLed = OFF;
 
